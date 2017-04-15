fe0236e2298818988b599e6ab028f50365335bfa
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-04-15 22:58:39 +0200

    Disk

--- start.sh
+++ start.sh
@@ -4,9 +4,19 @@
 # Creat encrypted disk images with LUKS, the lazy way.
 # Read the README.md for more info, but you will find more info here below.
 # By running this script you agree to the license terms.
-# -----------------------------------------------------------------------------------
+# Config ----------------------------------------------------------------------------
 LALUVER="0.1"
 LALUNAM="LazyLudicr"
+# Function --------------------------------------------------------------------------
+laluclear () {
+    LALUVER=""
+    LALUNAM=""
+    LALUKEY=""
+}
+laluclr () {
+    LALUKEY=""
+}
+# -----------------------------------------------------------------------------------
 while :; do
     clear
     echo "$LALUNAM v$LALUVER"
@@ -20,24 +30,32 @@ while :; do
     case "$LALUKEY" in
         1)
             clear
-            fallocate -l 512M enc.iso
-            sudo cryptsetup -y luksFormat enc.iso
-            sudo cryptsetup luksOpen enc.iso encVolume
-            sudo mkfs.ext4 /dev/mapper/encVolume
-            sudo e2label /dev/mapper/encVolume "diskname"
-            sudo cryptsetup luksClose encVolume
+            echo "$LALUNAM v$LALUVER"
+            echo ""
+            read -s -p "Enter disk image size: " LALUIMS
+            read -s -p "Enter disk image name: " LALUDSK
+            read -s -p -n1 "(M)B or (T)B: " LALUMOT
+            fallocate -l $LALUIMS$LALUMOT $LALUDSK.iso
+            sudo cryptsetup -y luksFormat $LALUDSK.iso
+            sudo cryptsetup luksOpen enc.iso LazyLudicr
+            sudo mkfs.ext4 /dev/mapper/LazyLudicr
+            sudo e2label /dev/mapper/LazyLudicr "$LALUDSK"
+            sudo cryptsetup luksClose LazyLudicr
             echo ""
             echo "All done..."
             read -p "Press (the infamous) any key to continue... " -n1 -s
+            laluclr
         ;;
         [qQ])
             clear
+            laluclear
             break
         ;;
         *)
             clear
             echo "Wrong option!"
             read -p "Press (the infamous) any key to continue... " -n1 -s
+            laluclr
         ;;
     esac
 done
