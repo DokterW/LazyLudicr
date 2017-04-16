fe3346ec335aaf37d94fffdee52de468a4ad2c97
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-04-16 12:27:41 +0200

    Lazy

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LazyLudicr v0.1
 # Made by Dr. Waldijk
-# Creat encrypted disk images with LUKS, the lazy way.
+# Create encrypted disk images with LUKS, the lazy way.
 # Read the README.md for more info, but you will find more info here below.
 # By running this script you agree to the license terms.
 # Config ----------------------------------------------------------------------------
